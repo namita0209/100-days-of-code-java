fe48a41f219061f9aac7700e5e8e5b102f7f8fb7
Namita Mishra <namita@example.com>
2025-12-31 14:44:11 +0530

    Day 02: Array sum and even-odd count

---
+++ Day02_EvenOddCount.java
@@ -0,0 +1,19 @@
+public class Day02_EvenOddCount {
+    public static void main(String[] args) {
+
+        int[] arr = {1, 2, 3, 4, 5, 6};
+        int even = 0, odd = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 == 0) {
+                even++;
+            } else {
+                odd++;
+            }
+        }
+
+        System.out.println("Even numbers count: " + even);
+        System.out.println("Odd numbers count: " + odd);
+    }
+}
+
